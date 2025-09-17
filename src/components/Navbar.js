8c0c8944fa9d408df94f16d326575423b601eace
Sim Joo Pee <sjpee@ilts.com>
2025-09-17 16:42:06 +0800

    Fix responsive logo in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,16 +7,18 @@ export default function Navbar() {
       <div className="max-w-6xl mx-auto flex items-center justify-between px-6 py-4">
         {/* Logo */}
         <Link href="/" className="flex items-center">
-          <Image
-            src="/images/LOGO-cropped2.svg"
-            alt="Logo"
-            width={180}
-            height={80}
-            className="object-contain cursor-pointer"
-            unoptimized
-         />
-          </Link>
-         {/* Menu */}
+          <div className="relative h-10 w-28 md:h-14 md:w-36 lg:h-16 lg:w-44">
+            <Image
+              src="/images/LOGO-cropped2.svg"
+              alt="Logo"
+              fill
+              className="object-contain cursor-pointer"
+              unoptimized
+            />
+          </div>
+        </Link>
+
+        {/* Menu */}
         <div className="flex space-x-8 items-center">
           <Link href="/" className="text-white hover:text-gray-400">Home</Link>
           <Link href="/projects" className="text-white hover:text-gray-400">Projects</Link>
