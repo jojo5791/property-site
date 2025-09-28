0b799ed696f188b3ba287fd7395f4aaba8649b5c
Sim Joo Pee <sjpee@ilts.com>
2025-09-28 13:58:45 +0800

    Fix error rib

--- src/app/page.js
+++ src/app/page.js
@@ -141,9 +141,10 @@ export default function Home() {
                   className="group relative overflow-hidden rounded-2xl shadow-lg"
                 >
                {/* Coming Soon Ribbon */}
-		<div className="absolute top-4 -left-12 bg-yellow-400 text-black font-bold text-xs md:text-sm px-20 py-2 transform -rotate-45 shadow-md z-20 tracking-wide whitespace-nowrap">
-		  Coming Soon
-		</div>
+		<div className="absolute top-6 -left-16 bg-yellow-400 text-black font-bold text-xs md:text-sm px-20 py-2 transform -rotate-45 shadow-md z-20 tracking-wide whitespace-nowrap">
+  Coming Soon
+</div>
+
 
                   <Image
                     src={project.image}
