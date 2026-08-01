49b753598a806edf877c86d61458c93e18b274b2
JaneD <70992666+jojo5791@users.noreply.github.com>
2026-08-01 20:00:13 +0800

    Update next.config.mjs 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,1 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Tells Vercel to allow production builds to finish cleanly even if 
-  // the template has older internal structural linting warnings
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  // Tells Vercel to ignore non-critical code type errors during compilation
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-};
-
-module.exports = nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
